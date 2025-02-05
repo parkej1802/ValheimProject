3b3ad82704189bf681776f3a22c0b400760271f5
parkej1802 <uijin.park1@ucalgary.ca>
2025-02-05 16:53:07 +0900

    Initialize

---
+++ Valheim/Source/Valheim/Valheim.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Valheim : ModuleRules
+{
+	public Valheim(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
